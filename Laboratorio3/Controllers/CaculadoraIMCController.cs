b080e1dc6d4bcf11ee0d83fe04d3c51616bd64a9
Jonathan_Navarro <jonathan.navarro00@outlook.es>
2021-09-07 15:49:57 -0600

    Final de laboratorio 3.

---
+++ Laboratorio3/Controllers/CaculadoraIMCController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using Laboratorio3.Models;
+using System;
+using System.Collections.Generic;
+using System.Web;
+namespace Laboratorio3.Controllers { 
+    public class CalculadoraIMCController : Controller {
+           public ActionResult ResultadoIMC() {
+                PersonaModel persona = new PersonaModel(1, "El bicho", 84.0, 1.87);
+                double IMC = persona.Peso / (persona.Estatura * persona.Estatura);
+                ViewBag.IMC = IMC;
+                ViewBag.persona = persona;
+                return View();
+            }
+
+
+        public ActionResult ResultadosAleatoriosIMC() {
+            List<PersonaModel> lista  =new  List<PersonaModel>();
+            List<double> IMCLista = new List<double>();
+            Random random = new Random();
+            for (int i = 0; i < 30; i++) {
+                lista.Add(new PersonaModel(i, "Sujeto de pruebas", random.NextDouble() * (150 - 20) + 20, random.NextDouble() * (2 - 1) + 1));
+                IMCLista.Add(lista[i].Peso / (lista[i].Estatura * lista[i].Estatura));
+            }
+            ViewBag.IMCL = IMCLista;
+            ViewBag.lista = lista;
+            return View();
+        }
+    }
+}
---
+++ Laboratorio3/Models/PersonaModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio3.Models
+{
+    public class PersonaModel
+    {
+        public string Nombre { get; set; }
+        public int Id { get; set; }
+        public double Peso { get; set; }
+        public double Estatura { get; set; }
+        public PersonaModel() {
+            this.Nombre = "Sin nombre";
+            this.Id = 0;
+            this.Peso= 0.0;
+            this.Estatura = 0.0;
+        }
+        public PersonaModel(int Id, string Nombre, double Peso, double Estatura) {
+            this.Nombre = Nombre;
+            this.Id = Id;
+            this.Peso = Peso;
+            this.Estatura = Estatura;
+
+        }
+    }
+}
